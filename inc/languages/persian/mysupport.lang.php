b49288f4138aa229aa039adee9fe392336762325
ATofighi <alirtofighim@gmail.com>
2014-03-25 03:44:06 +0330

    آپلود فایل ها

---
+++ inc/languages/persian/mysupport.lang.php
@@ -0,0 +1,174 @@
+<?php
+/**
+ * MySupport 0.4.2 - Language File
+ * © MattRogowski 2010
+ * http://mattrogowski.co.uk
+ * You may edit for your own personal forum but must not redistribute it in any form without my permission.
+**/
+
+$l['mysupport'] = "پشتیبانی من";
+
+$l['markas'] = "علامت‌گذاری به عنوان:";
+$l['solved'] = "حل‌شده";
+$l['solved_close'] = "حل‌شده (و بستن)";
+$l['not_solved'] = "حل‌نشده";
+$l['technical'] = "فنی";
+$l['not_technical'] = "غیرفنی";
+$l['solved_time'] = "حل‌شده: {1}";
+$l['technical_time'] = "فنی: {1}";
+
+$l['status_change_success'] = "وضعیت از {1} به {2} تغییرکرد.";
+$l['status_change_success_multi'] = "وضعیت {1} موضوع به {2} تغییرکرد.";
+$l['status_change_mod_log'] = "موضوع به عنوان {1} علامت‌گذاری شد.";
+$l['status_change_mod_log_multi'] = "{1} موضوع به عنوان {2} علامت‌گذاری شد.";
+
+$l['error_not_mysupport_forum'] = "پشتیبانی‌من برای این انجمن قابل‌استفاده نیست.";
+$l['error_same_status'] = "این موضوع قبلا به عنوان  {1} علامت‌گذاری شده‌است.";
+$l['no_permission_mark_solved'] = "شما دسترسی برای علامت گذاری این موضوع به عنوان حل‌شده را ندارید.";
+$l['no_permission_mark_solved_multi'] = "شما دسترسی برای علامت‌گذاری این موضوع‌ها به عنوان حل‌شده را ندارید.";
+$l['no_permission_mark_notsolved'] = "شما دسترسی برای علامت گذاری این موضوع به عنوان حل‌نشده را ندارید.";
+$l['no_permission_mark_notsolved_multi'] = "شما دسترسی برای علامت‌گذاری این موضوع‌ها به عنوان حل‌نشده را ندارید.";
+$l['no_permission_mark_solved_close'] = "شما دسترسی برای علامت گذاری این موضوع به عنوان حل‌شده و بستن‌آن را ندارید.";
+$l['no_permission_mark_solved_close_multi'] = "شما دسترسی برای علامت‌گذاری این موضوع‌ها به عنوان حل‌شده و بستن‌آن‌ها را ندارید.";
+$l['no_permission_mark_technical'] = "شما دسترسی برای علامت گذاری این موضوع به عنوان فنی را ندارید.";
+$l['no_permission_mark_technical_multi'] = "شما دسترسی برای علامت گذاری این موضوع‌ها به عنوان فنی را ندارید.";
+$l['no_permission_mark_nottechnical'] = "شما دسترسی برای علامت گذاری این موضوع به عنوان غیرفنی را ندارید.";
+$l['no_permission_mark_nottechnical_multi'] = "شما دسترسی برای علامت گذاری این موضوع‌ها به عنوان غیرفنی را ندارید.";
+$l['no_mysupport_threads_selected'] = "شما هیچ موضوعی را در انجمن‌های پشتیبانی‌من انتخاب نکردید.";
+$l['mysupport_thread'] = "موضوع";
+$l['mysupport_threads'] = "موضوع";
+$l['technical_global'] = "{1} technical {2} requiring attention.";
+$l['technical_forum'] = "{1} technical {2} requiring attention in this forum.";
+$l['technical_global_forum'] = "{1} technical {2} requiring attention, with {3} in this forum.";
+
+$l['thread_list_title_solved'] = "Your Support Threads";
+$l['thread_list_title_tech'] = "Technical Threads";
+$l['thread_list_title_assign'] = "Your Assigned Threads";
+$l['thread_list_heading_solved'] = "Your support threads.";
+$l['thread_list_heading_solved_latest'] = "Your latest support threads.";
+$l['thread_list_heading_solved_new'] = "Your support threads with activity since your last visist.";
+$l['thread_list_heading_tech'] = "Threads requiring technical attention.";
+$l['thread_list_heading_assign'] = "Your assigned threads.";
+$l['thread_list_heading_solved_forum'] = "Your support threads in '{1}'.";
+$l['thread_list_heading_tech_forum'] = "Threads requiring technical attention in '{1}'.";
+$l['thread_list_heading_assign_forum'] = "Your assigned threads in '{1}'.";
+$l['thread_list_view_all_solved'] = "View all your support threads.";
+$l['thread_list_view_all_tech'] = "View all technical threads.";
+$l['thread_list_view_all_assign'] = "View all assigned threads.";
+$l['thread_list_link_solved'] = "View all your support threads in '{1}'";
+$l['thread_list_link_tech'] = "View all technical threads in '{1}'";
+$l['thread_list_link_assign'] = "View all assigned threads in '{1}'";
+$l['thread_list_threadauthor'] = "Thread / Author:";
+$l['thread_list_time_solved'] = "Status:";
+$l['thread_list_time_tech'] = "Marked Technical:";
+$l['thread_list_time_assign'] = "Status:";
+$l['thread_list_lastpost'] = "Last Post";
+$l['thread_list_newthreads'] = "{1} of your support threads have had activity since your last visit.";
+$l['thread_list_no_results'] = "No threads matched your criteria.";
+$l['mysupport_by'] = "by {1}";
+$l['mysupport_assigned_by'] = "assigned by {1}";
+
+$l['thread_list_stats_overview_heading_solved'] = "Overview of all your support threads.";
+$l['thread_list_stats_overview_heading_solved_forum'] = "Overview of all your support threads in '{1}'.";
+$l['thread_list_stats_overview_heading_tech'] = "Overview of all support threads.";
+$l['thread_list_stats_overview_heading_tech_forum'] =  "Overview of all support threads in '{1}'.";
+$l['thread_list_stats_overview'] = "Total Support Threads: <strong>{1}</strong> Solved Threads: <strong>{2}</strong> Unsolved Threads: <strong>{3}</strong> Technical Threads: <strong>{4}</strong>";
+
+$l['mysupport_wol_support'] = "Viewing their <a href=\"usercp.php?action=supportthreads\">Support Threads</a>";
+$l['mysupport_wol_technical'] = "Viewing Technical Threads";
+$l['mysupport_wol_support_denial'] = "Managing Support Denial";
+$l['mysupport_wol_support_denial_deny'] = "Denying Support";
+
+$l['bestanswer_img_alt'] = "Mark as best answer";
+$l['bestanswer_img_title'] = "Mark this post as the best answer in the thread.";
+$l['bestanswer_redirect'] = "Post successfully marked as the best answer.";
+$l['unbestanswer_img_alt'] = "Unmark as best answer";
+$l['unbestanswer_img_title'] = "Unmark this post as the best answer in the thread.";
+$l['unbestanswer_redirect'] = "Post successfully unmarked as the best answer.";
+$l['bestanswer_not_enabled'] = "You cannot mark this post as the best answer as the Administrator has not enabled this feature.";
+$l['bestanswer_invalid_forum'] = "This post is not from a thread within a valid forum.";
+$l['bestanswer_not_author'] = "You cannot mark this post as the best answer as you did not author the thread.";
+$l['solve_and_highlight'] = "Post successfully highlighted.<br /><br />Status changed from {1} to {2}.";
+$l['jump_to_bestanswer'] = "Jump to the post that solved this thread.";
+
+$l['assigned'] = "Assigned";
+$l['assigned_toyou'] = "Assigned to you";
+$l['assign_to'] = "Assign to:";
+$l['assign_to_nobody'] = "Nobody";
+$l['assigned_to_success'] = "Assigned to {1}.";
+$l['assigned_to_success_multi'] = "Assigned {1} thread(s) to {2}.";
+$l['unassigned_from_success'] = "Unassigned from {1}.";
+$l['unassigned_from_success_multi'] = "Unassigned {1} thread(s).";
+$l['assign_no_perms'] = "You do not have permission to assign threads.";
+$l['assign_invalid'] = "You have chosen an invalid user to assign this thread to.";
+$l['assign_solved'] = "You cannot assign a thread which has been solved. To assign it you need to mark it as not solved as well.";
+$l['assign_global'] = "You've been assigned {1} {2}.";
+$l['assign_forum'] = "You've been assigned {1} {2}, with {3} in this forum.";
+$l['assign_pm_subject'] = "You have been assigned a new thread";
+$l['assigned_by'] = "You were assigned this thread by [url={1}]{2}[/url].";
+$l['assigned_by_you'] = "You assigned this thread to yourself.";
+$l['assign_pm_message'] = "{1},
+
+You have been assigned a new thread in [url={2}]{3}[/url] titled [url={4}]{5}[/url]. {6}
+
+You can see all of your assigned threads by clicking [url={7}/usercp.php?action=assignedthreads]here[/url].";
+
+$l['priority'] = "Priority:";
+$l['priority_none'] = "None";
+$l['priority_change_success_to'] = "Priority changed to {1}.";
+$l['priority_change_success_to_multi'] = "Priority of {1} threads(s) changed to {2}.";
+$l['priority_change_success_fromto'] = "Priority changed from {1} to {2}.";
+$l['priority_remove_success'] = "Priority of {1} removed.";
+$l['priority_remove_success_multi'] = "Priority removed from {1} thread(s).";
+$l['priority_no_perms'] = "You do not have permission to give a thread a priority.";
+$l['priority_solved'] = "You cannot give a priority to a thread which has been solved. To give a priority to it you need to mark it as not solved as well.";
+$l['priority_invalid'] = "You have selected an invalid priority.";
+
+$l['category'] = "Category:";
+$l['category_none'] = "None";
+$l['category_change_success_to'] = "Category changed to {1}.";
+$l['category_change_success_to_multi'] = "Category of {1} thread(s) changed to {2}.";
+$l['category_change_success_fromto'] = "Category changed from {1} to {2}.";
+$l['category_remove_success'] = "Category of {1} removed.";
+$l['category_remove_success_multi'] = "Category removed from {1} thread(s).";
+$l['category_invalid'] = "You have selected an invalid category.";
+
+$l['support_denial'] = "Support Denial";
+$l['deny_support'] = "Deny Support";
+$l['deny_support_to'] = "Deny support to {1}";
+$l['deny_support_to_user'] = "Deny support to a user";
+$l['deny_support_desc'] = "If a user is denied support, they will not be able to create new threads in MySupport forums. A message will appear in all their current posts in MySupport forums to alert other users that the user has been denied support. If you choose a reason for denial below, it will display when a user tries to make a new thread in a MySupport forum.";
+$l['denied_support'] = "This user has been denied support.";
+$l['denied_support_click_to_edit_revoke'] = "(Click to edit/revoke)";
+$l['deny_support_success'] = "You have successfully denied support to {1}.";
+$l['deny_support_revoke_success'] = "You have successfully revoked denial of support from {1}.";
+$l['support_denial_reasons_none'] = "None";
+$l['support_denial_reason_invalid_reason'] = "You have selected an invalid reason.";
+$l['support_denial_reason_invalid_user'] = "You have selected an invalid user.";
+$l['reason'] = "Reason";
+$l['revoke'] = "Revoke";
+$l['revoke_from'] = "Revoke denial of support from {1}";
+$l['revoke_denial'] = "Revoke denial of support.";
+$l['support_denial_not_enabled'] = "Support denial is not enabled.";
+$l['deny_support_mod_log'] = "Denied support to {1}.";
+$l['deny_support_mod_log_reason'] = "Denied support to {1}. Reason: {2}";
+$l['deny_support_revoke_mod_log'] = "Revoked support denial from {1}.";
+$l['users_denied_support'] = "Users denied support";
+$l['support_denial_reason'] = "Support Denial Reason";
+$l['support_denial_user'] = "Support Denied By";
+$l['support_denial_no_reason'] = "No reason given.";
+$l['support_denial_no_users'] = "No users have been denied support.";
+
+$l['deniedsupport'] = "You cannot start a new support thread here as you have been denied support on these forums.";
+$l['deniedsupport_reason'] = "Reason: {1}";
+
+$l['mysupport_options'] = "MySupport Options";
+$l['mysupport_show_as_text'] = "Show thread statuses as text??";
+
+$l['error_no_action'] = "Please select a new status, user to assign the thread to, priority or category.";
+
+$l['mysupport_activated'] = "MySupport activated successfully, please configure the settings below.";
+
+$l['mysupport_just_now'] = "هم‌اکنون";
+$l['mysupport_ago'] = "قابل";
+?>
